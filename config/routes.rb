0719c93b0f2e4ede42f81a8fedf97d903a59dc99
Shunsuke-Itoh <shunsuke.ito.1019@gmail.com>
2020-02-11 17:31:17 +0900

    create_db_&_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :kari, only: :index
   resources :users
+  root 'top_page#index'
 end
