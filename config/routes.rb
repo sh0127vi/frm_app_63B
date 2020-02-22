84a350710d12754b9aa735a1c77397f6ba37ed06
sh0127vi <0530icehockey@gmail.com>
2020-02-22 20:24:24 +0900

    routes.rbの不要な文字の削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :kari, only: :index
-=======
   resources :users
->>>>>>> 9265cfcf23e8f9f6ede55028e3370a1cad576949
 end
