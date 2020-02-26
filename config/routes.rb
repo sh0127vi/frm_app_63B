58b1c99ac527f52530681e654a2265370bcfa4b1
h-takuya21 <h.t02011989@gmail.com>
2020-02-27 08:08:35 +0900

    マスター適用2

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  root to: "products#new"
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products, only: :new
-  resources :users
+  
   root 'top_page#index'
-  get 'users/index'
-  root 'users#index'
-end
-  root to: "users#index"
+  get 'users' => 'users#index'
   get 'users/logout' => 'users#logout'
   get 'users/card' => 'users#card'
   get 'users/users/card_add' => 'users#card_add'
