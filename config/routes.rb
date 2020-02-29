19228d9d30d3d3bf5eec8b121a13b33d1fafd4ff
Hastur9999 <knt_msr@kansai.me>
2020-02-29 22:46:41 +0900

    Merge branch 'master' of https://github.com/sh0127vi/frm_app_63B into login_page(server-side)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: "products#new"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :kari, only: :index
   resources :products, only: :new
   resources :users
   root 'top_page#index'
