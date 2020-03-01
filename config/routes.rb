b5f3367d65cb368399c0f2e54ba24288400f02e4
h-takuya21 <h.t02011989@gmail.com>
2020-03-01 09:23:40 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  
+  root 'top_page#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :products, only: :new
   
-  root 'top_page#index'
-  get 'users' => 'users#index'
-  get 'users/logout' => 'users#logout'
-  get 'users/card' => 'users#card'
-  get 'users/users/card_add' => 'users#card_add'
-
-
+  devise_scope :user do
+    get "users", :to => "users#index"
+    get "users/logout", :to => "users#logout"
+    get "users/card", :to => "users#card" 
+    get "users/users/card_add", :to => "users#card_add" 
+  end
 end
