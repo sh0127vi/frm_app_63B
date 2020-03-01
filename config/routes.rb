c11d59a9592e43a4fd099965cec8871a5a4c2a94
sh0127vi <0530icehockey@gmail.com>
2020-03-01 19:56:05 +0900

    routes.rbの3行目コメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: "products#index"
-  devise_for :users
+  #devise_for :users
   devise_scope :user do
     get "users", to: "users#index"
     get "users/logout", to: "users#logout"
     get "users/card", to: "users#card" 
-    get "users/users/card_add", to: "users#card_add" 
+    get "users/card_add", to: "users#card_add" 
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
