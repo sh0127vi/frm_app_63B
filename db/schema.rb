f9cd83053c0251c92d8cdd856c51ef98ce3c3e22
Hastur9999 <knt_msr@kansai.me>
2020-03-02 22:50:23 +0900

    add_column user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_085458) do
+ActiveRecord::Schema.define(version: 2020_03_02_134652) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "f_name_kana", null: false
@@ -102,6 +102,13 @@ ActiveRecord::Schema.define(version: 2020_03_01_085458) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", null: false
+    t.string "f_name_kana", null: false
+    t.string "l_name_kana", null: false
+    t.string "f_name", null: false
+    t.string "l_name", null: false
+    t.integer "birthday", null: false
+    t.integer "phone_number", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
