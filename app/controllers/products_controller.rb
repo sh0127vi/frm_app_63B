c1c8b7d5caed39a43ba81a50314df0b6cf311d36
h-takuya21 <h.t02011989@gmail.com>
2020-03-02 18:15:04 +0900

    routes,header,footer実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,9 @@ class ProductsController < ApplicationController
 
   def new 
   end
+
+  def show
+  end
   
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: [:new, :index]
+  resources :products, only: [:new, :index, :show]
   resources :users
 end
