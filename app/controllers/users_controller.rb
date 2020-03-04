07d1ac3ac7840431cc11ec9b2a536c137fcbd5a5
Hastur9999 <knt_msr@kansai.me>
2020-03-04 20:51:14 +0900

    fix user_controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.build_address
+    @user.save
+  end
+
   def edit
   end
 
@@ -8,12 +17,26 @@ class UsersController < ApplicationController
   def show
   end
 
-  def index
-  end
-  
-  def edit
-  end
+  private
 
-  def update
+  def user_params
+    params.require(:user).permit(:sign_up, keys: [
+        :nickname, 
+        :f_name_kana, 
+        :l_name_kana,
+        :f_name,
+        :l_name,
+        :birthday,
+        :phone_number,
+        address_attributes: [:f_name_kana,
+                              :l_name_kana,
+                              :f_name,
+                              :l_name,
+                              :postal_code,
+                              :prefecture,
+                              :city,
+                              :street,
+                              :building
+    ]])
   end
 end
