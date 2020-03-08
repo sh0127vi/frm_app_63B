6fbada2071172dce1e9ee0293add9e6f4968fa41
h-takuya21 <h.t02011989@gmail.com>
2020-03-08 16:43:21 +0900

    routesコメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
     get "card_add", to: "users#card_add" 
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: [:new, :index] do
+  
+  resources :products, only: [:new, :index, :show] do
     collection do
       get "buy"
     end
