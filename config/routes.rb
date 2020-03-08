f534c021a051e889f2bcebf8f60f8cea02bbad15
sh0127vi <0530icehockey@gmail.com>
2020-03-08 12:49:00 +0900

    routingpath戻し

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get "users", to: "users#index"
-    get "users/logout", to: "users#logout"
-    get "users/card", to: "users#card" 
-    get "users/card_add", to: "users#card_add" 
+    get "logout", to: "users#logout"
+    get "card", to: "users#card" 
+    get "card_add", to: "users#card_add" 
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
