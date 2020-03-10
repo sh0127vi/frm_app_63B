d13e72c726de9f9bd1def95768cd5aef0bdee38c
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-03-11 01:38:35 +0900

    set-cteate_action-and-strong_parameter
    クリエイトアクションとストロングパラメーターの記述をした

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,11 +8,25 @@ class ProductsController < ApplicationController
     @product.images.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
   end
 
   def buy
   end
   
+  private
+
+  def product_params
+    params.require(:product).permit(:user_id, :category_id, :name, :detail, :price, :condition, :city, :delivery, :fee_payer, :delivery_area, images_attributes: [:images])
+  end
 end
 
