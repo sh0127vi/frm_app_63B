99f849d2db80964f63fc2f55899e6efae4516b84
h-takuya21 <h.t02011989@gmail.com>
2020-03-10 13:09:03 +0900

    routes,users_controller修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   root to: "products#index"
   devise_for :users
-  devise_scope :user do
-    get "users", to: "users#index"
+  
+
+  resources :users, only: [:index, :logout, :card, :card_add] do
+    collection do
+    get "index", to: "users#index"
     get "logout", to: "users#logout"
     get "card", to: "users#card" 
     get "card_add", to: "users#card_add" 
+    end
   end
 
-  
-  resources :products, only: [:new, :index, :show] do
+  resources :products, only: [:new, :index, :show, ] do
     collection do
       get "buy"
     end
   end
   
-
-  resources :users
 end
