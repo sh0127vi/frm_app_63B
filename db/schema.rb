fd9cb1b7b670443bd463f09243ec4bec2f4796aa
Hastur9999 <knt_msr@kansai.me>
2020-03-14 16:30:51 +0900

    電話番号をユーザーテーブルからアドレステーブルへ変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_113539) do
+ActiveRecord::Schema.define(version: 2020_03_14_072817) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "f_name_kana", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_113539) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
@@ -108,7 +109,6 @@ ActiveRecord::Schema.define(version: 2020_03_04_113539) do
     t.string "f_name", null: false
     t.string "l_name", null: false
     t.date "birthday", null: false
-    t.string "phone_number", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
