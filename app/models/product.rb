faff58b4ce461dde40fe6acf9259f713109fd5ef
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-03-20 14:43:16 +0900

    image_validation-product_model
    プロダクトモデルに画像の拡張子を限定するバリデーションを書きました。

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ApplicationRecord
   # アソシエーション
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
+  validate :image_validate
+
+  private
+
+  def image_validate
+    images.each do |image|
+      if !image.content_type.in?(%('image/jpec image/png'))
+        errors.add(:images, '画像ファイル以外のファイルを送付しないでください')
+      end
+    end
+  end
+
 end
