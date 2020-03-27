5c79af60571e7da27b8ad2f2e2a1d1fc03267cfb
Shunsuke-Itoh <shunsuke.ito.1019@gmail.com>
2020-03-27 19:01:04 +0900

    create_credit_card_controller

---
+++ app/controllers/credit_card_controller.rb
@@ -0,0 +1,11 @@
+class CreditCardController < ApplicationController
+
+  require "payjp"
+
+  def new
+
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root to: "products#index_Top_page"
   devise_for :users, controllers: {
     registrations: 'users/registrations',
