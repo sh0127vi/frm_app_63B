30c6a6a132e356c60ecbdc45824624b6d2a8e2cd
sh0127vi <0530icehockey@gmail.com>
2020-03-30 16:48:26 +0900

    console.log削除

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,12 +1,10 @@
 $(function() {
- 
   $(".each-image").first().addClass("active");
   $(".each-image").click(function() {
     let img = $(this).attr("src");
     $('.active-image').attr("src", img).hide().fadeIn();
     $('.each-iamge').removeClass('active');
     $(this).parent().addClass("active");
-    console.log(this);
   });
 
 });
