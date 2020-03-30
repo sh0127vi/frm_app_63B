a42132ab26e9d0fee7f1d25adf8f9df42006906d
sh0127vi <0530icehockey@gmail.com>
2020-03-30 14:57:14 +0900

    条件分岐と画像の選択表示

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,12 +1,12 @@
 $(function() {
-
-   console.log("hello");
+ 
+  $(".each-image").first().addClass("active");
   $(".each-image").click(function() {
-    let actImg = $(this).attr("src");
-    $(".active-image").attr({src:actImg});
-    $(".active-image").hide();
-    $(".active-image").fadeIn();
-      return false;
+    let img = $(this).attr("src");
+    $('.active-image').attr("src", img).hide().fadeIn();
+    $('.each-iamge').removeClass('active');
+    $(this).parent().addClass("active");
+    console.log(this);
   });
 
 });
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,12 @@ class Product < ApplicationRecord
   has_many   :likes_users, through: :user, source: :user
   has_one    :purchase
   has_many   :comments
+
+  def previous
+    Product.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def next
+    Product.where("id > ?", self.id).order("id ASC").first
+  end
 end
