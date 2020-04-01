adcdaa31afca5fc8d42ec6456448a1cad8141a03
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-04-01 22:31:36 +0900

    fix-product.rb
    productモデルのアソシエーションを修正しました。

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   # アソシエーション
-  accepts_nested_attributes_for :images, allow_destroy: true
+
   belongs_to :user
   has_many   :images,      dependent: :destroy
   belongs_to :category,    dependent: :destroy
@@ -8,6 +8,7 @@ class Product < ApplicationRecord
   has_many   :likes_users, through: :user, source: :user
   has_one    :purchase
   has_many   :comments
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   validate :image_validate
 
