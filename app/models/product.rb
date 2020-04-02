61141ff48e0032cff4251f0ea0ebc69ddadf6794
Hastur9999 <knt_msr@kansai.me>
2020-04-02 11:52:55 +0900

    現在ないモデルのアソシエーション削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many   :images,      dependent: :destroy
   belongs_to :category
-  has_many   :likes,       dependent: :destroy
-  has_many   :likes_users, through: :user, source: :user
   has_one    :purchase
   has_many   :comments
 
