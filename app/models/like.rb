5b924818a5b2d1c7ceb1a8c06105ac640ca24f35
Hastur9999 <knt_msr@kansai.me>
2020-04-04 20:06:16 +0900

    fix model

---
+++ app/models/like.rb
@@ -0,0 +1,7 @@
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :product, counter_cache: :likes_count
+
+  validates :user_id, :product_id, presence: true
+  validates_uniqueness_of :product_id, scope: :user_id
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many   :images,      dependent: :destroy
   belongs_to :category
   has_one    :purchase
+  has_many   :images,      dependent: :destroy
   has_many   :comments
+  has_many   :likes, dependent: :destroy
+  has_many   :like_users, through: :likes, source: :user
 
   def previous
     Product.where("id < ?", self.id).order("id DESC").first
@@ -11,5 +13,9 @@ class Product < ApplicationRecord
 
   def next
     Product.where("id > ?", self.id).order("id ASC").first
+  end
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
   end
 end
