9ff8d2be75a509bb7801da640ddc355a83c879c9
sh0127vi <0530icehockey@gmail.com>
2020-04-04 16:31:11 +0900

    seeds5

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,27 @@ Product.create!(
   user_id: '6',
   category_id: '1'
 )
-
 Image.create!(
-  product_id: '1',
+  product_id: '6',
   images: 'https://askul.c.yimg.jp/img/product/3L1/4004438_3L1.jpg'
 )
+
+
+Product.create!(
+  name: '本',
+  detail: '自己啓発',
+  price: '2000',
+  condition: '中古',
+  city: '滋賀',
+  delivery: '100',
+  fee_payer: '100',
+  delivery_area: '草津',
+  user_id: '6',
+  category_id: '1'
+)
+
+Image.create!(
+  product_id: '7',
+  images: 'https://illust8.com/wp-content/uploads/2019/04/thick-book_illust_3551.png'
+)
+
