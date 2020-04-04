85319ee1119722d5c886a4fc5d192f3ed74a3e06
Hastur9999 <knt_msr@kansai.me>
2020-04-05 00:53:23 +0900

    fix like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :product, counter_cache: :likes_count
 
-  validates :user_id, :product_id, presence: true
+  validates :user_id, presence: true
+  validates :product_id, presence: true
   validates_uniqueness_of :product_id, scope: :user_id
 end
