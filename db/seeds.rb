46352f272f03c352f74c23ffd6296b14f103bb7e
sh0127vi <0530icehockey@gmail.com>
2020-04-04 15:42:05 +0900

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,22 @@
-2.times do |n|
-  Product.create!(
-    user_id:     "1"
-    category_id: "1"
-    name:        "test#{n + 1}",
-    price:       "#{(n + 1)*1000}"
 
-  )
-
-2.times do |n|
-  Product.create!(
-    user_id:     "1"
-    category_id: "1"
-    name:        "test売り切れ#{n + 1}",
-    price:       "#{(n + 1)*1000}"
-  )
-
-Image.create!(
-  [
-    {
-      images:     "https://s3-ap-northeast-1.amazonaws.com/mercarimaster/uploads/captured_image/content/13/a007.png"
-      product_id: "1"
-    },
-    {
-      images:     "https://s3-ap-northeast-1.amazonaws.com/mercarimaster/uploads/captured_image/content/10/a004.png"
-      product_id: "3"
-    }
-  ]
+Category.create(
+  name: 'メンズ'
 )
 
-Purchase.create!(
-  [
-    {
-      buyer_id:   2
-      product_id: 3
-    },
-    {
-      buyer_id:   2
-      product_id: 4
-    }
-  ]
+Product.create!(
+  name: 'サバ',
+  detail: '新鮮',
+  price: '2500',
+  condition: '刺身',
+  city: '滋賀',
+  delivery: '100',
+  fee_payer: '100',
+  delivery_area: '草津',
+  user_id: '1',
+  category_id: '1'
+)
+
+Image.create!(
+  product_id: '1',
+  images: 'https://askul.c.yimg.jp/img/product/3L1/4004438_3L1.jpg'
 )
