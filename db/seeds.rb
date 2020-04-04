1ef3fab1349d60f362356b7eee00614390fdbb6a
sh0127vi <0530icehockey@gmail.com>
2020-04-04 16:43:52 +0900

    seeds8

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,62 @@
 Product.create!(
-  name: 'サバ',
-  detail: '自己啓発',
-  price: '2000',
-  condition: '中古',
-  city: '滋賀',
-  delivery: '100',
-  fee_payer: '100',
-  delivery_area: '草津',
-  user_id: '6',
-  category_id: '1'
+  [
+    {
+      name: 'サバ',
+      detail: '自己啓発',
+      price: '2000',
+      condition: '中古',
+      city: '滋賀',
+      delivery: '100',
+      fee_payer: '100',
+      delivery_area: '草津',
+      user_id: '6',
+      category_id: '1'
+    },
+    {
+      name: '本',
+      detail: '自己啓発',
+      price: '2000',
+      condition: '中古',
+      city: '滋賀',
+      delivery: '100',
+      fee_payer: '100',
+      delivery_area: '草津',
+      user_id: '6',
+      category_id: '1'
+    },
+    {
+      name: 'ペン',
+      detail: '自己啓発',
+      price: '2000',
+      condition: '中古',
+      city: '滋賀',
+      delivery: '100',
+      fee_payer: '100',
+      delivery_area: '草津',
+      user_id: '6',
+      category_id: '1'
+    }
+  ]
 )
 Image.create!(
-  product_id: '6',
-  images: 'https://askul.c.yimg.jp/img/product/3L1/4004438_3L1.jpg'
+  [
+    {
+      product_id: '7',
+      images: 'https://askul.c.yimg.jp/img/product/3L1/4004438_3L1.jpg'
+    },
+    {
+      product_id: '8',
+      images: 'https://illust8.com/wp-content/uploads/2019/04/thick-book_illust_3551.png'
+    },
+    {
+      product_id: '9',
+      images: 'https://images-na.ssl-images-amazon.com/images/I/614Bxu744KL._AC_SX466_.jpg'
+    },
+    {
+      product_id: '7',
+      images: 'https://www.kaldi.co.jp/ec/img/020/7039010149020_M_1m.jpg'
+    }
+  ]
 )
 
 
