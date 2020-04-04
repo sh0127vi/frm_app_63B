b19ffefbbedc98c7f784d1e468a695d2d209384d
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-04-04 17:55:32 +0900

    product.rb
    productモデルの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,15 +3,12 @@ class Product < ApplicationRecord
 
   belongs_to :user
   has_many   :images,      dependent: :destroy
-  belongs_to :category,    dependent: :destroy
-  has_many   :likes,       dependent: :destroy
+  belongs_to :category
+  belongs_to :brand, optional: true
   has_many   :likes_users, through: :user, source: :user
   has_one    :purchase
   has_many   :comments
   accepts_nested_attributes_for :images, allow_destroy: true
-
-  validate :image_validate
-
   def previous
     Product.where("id < ?", self.id).order("id DESC").first
   end
