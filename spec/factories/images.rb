3a0c5373177fcdace9b19342c0761750d1971522
sh0127vi <0530icehockey@gmail.com>
2020-04-05 17:47:37 +0900

    specをgitから外す

--- spec/factories/images.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-
-  factory :image do
-    association :product
-
-    images { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test_image.jpg')) }
-    
-  end
-end
