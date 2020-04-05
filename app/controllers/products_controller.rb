b727bb25a81e0b1fbcbba7fac8d9a3f59364274a
Kani2019 <seiya.k.w@icloud.com>
2020-04-05 11:20:34 +0900

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,6 +30,19 @@ class ProductsController < ApplicationController
     @category = Category.find((@product).category_id)
   end
 
+  # 以下全て、formatはjsonのみ
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+     #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
   def buy
   end
 
@@ -46,17 +59,5 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit( :name, :detail, :price,:category_id,:brand_id, :condition, :city, :fee_payer, :delivery, images_attributes: [:images]).merge(user_id: current_user.id)
-  end
-   # 以下全て、formatはjsonのみ
-   # 親カテゴリーが選択された後に動くアクション
-  def get_category_children
-    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-  end
-
-  # 子カテゴリーが選択された後に動くアクション
-  def get_category_grandchildren
-    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 end
