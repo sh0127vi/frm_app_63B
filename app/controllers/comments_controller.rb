4742eec7ef78ba23f099e33e59ef676eaef8f3f4
sh0127vi <0530icehockey@gmail.com>
2020-04-06 20:29:33 +0900

    flash message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,16 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     comment = Comment.create(comment_params)
     if comment.save
-      redirect_to product_path(@product)
+      redirect_to product_path(@product), notice: 'コメントが送信されました'
     else
-      render js: "alert('コメントの送信に失敗しました');"
+      flash.now[:alert] = 'コメントを入力してください'
+      redirect_to product_path(@product)
     end
   end
 
   private
-  
   def comment_params
     params.require(:comment).permit(:body).merge(user_id: current_user.id, product_id: params[:product_id])
   end
+
 end
